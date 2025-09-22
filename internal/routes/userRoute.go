49b4e055e8dac2d6e273e2a4642cb928c1e9c154
Dionisius951 <dionisius678@gmail.com>
2025-09-22 22:53:05 +0700

    setup routes

---
+++ internal/routes/userRoute.go
@@ -0,0 +1,11 @@
+package routes
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func userRouter(r *mux.Router) {
+	// userRoute := r.PathPrefix("/users").Subrouter()
+
+	// userRoute.HandleFunc("/login",controllers.LoginHandler()).Methods("POST")
+}
